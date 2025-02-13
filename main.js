6794c6503dde5c8ec4990ef4198f8f4d529dbfb2
¨luisantonio16¨ <luis16.16m@gmail.com>
2025-02-13 19:50:14 -0400

    moo

---
+++ main.js
@@ -0,0 +1,17 @@
+onload = () => {
+    document.body.classList.remove("container");
+  };
+
+
+  document.addEventListener('DOMContentLoaded', function() {
+    const titles = ['Para una mujer linda','y', 'Maravillosa'];
+    let index = 0;
+  
+    function changeTitle() {
+      document.getElementById('nombre').textContent = titles[index];
+      index = (index + 1) % titles.length;
+    }
+  
+    setInterval(changeTitle, 4000);
+  });
+  
